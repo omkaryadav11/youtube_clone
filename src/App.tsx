bf506e327aab251faba1f7bcad53c225cc87469e
omkaryadav11 <oy970831@gmail.com>
2024-02-12 20:44:05 +0530

    project set-up

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Search from './pages/Search';
+import Watch from './pages/Watch';
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+      <Route path='/search' element={<Search/>}/>
+        <Route path='/watch:id' element={<Watch/>}/>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
 }
-
-export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { AiOutlineClose, AiOutlineSearch } from "react-icons/ai";
+import {TiMicrophone} from 'react-icons/ti';
+import { BsYoutube, BsCameraVideo, BsBell } from "react-icons/bs";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoAppsSharp } from "react-icons/io5";
+export default function NavBar() {
+  return (
+    <div>
+      Navbar
+    </div>
+  )
+}
